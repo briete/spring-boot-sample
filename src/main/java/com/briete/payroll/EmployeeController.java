8c99eefcae035f5859fef6c26ccff236976f8fea
sato.naoya <sato.naoya@classmethod.jp>
2021-10-06 18:37:19 +0900

    ModelAssembler

--- src/main/java/com/briete/payroll/EmployeeController.java
+++ src/main/java/com/briete/payroll/EmployeeController.java
@@ -11,16 +11,17 @@ import java.util.stream.Collectors;
 @RestController
 public class EmployeeController {
     private final EmployeeRepository repository;
+    private final EmployeeModelAssembler assembler;
 
-    EmployeeController(EmployeeRepository repository) {
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     @GetMapping("/employees")
     CollectionModel<EntityModel<Employee>> all() {
-        List<EntityModel<Employee>> employees = repository.findAll().stream().map(employee -> EntityModel.of(employee,
-                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"))).collect(Collectors.toList());
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+                .map(assembler::toModel).collect(Collectors.toList());
         return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
 
     }
@@ -33,9 +34,7 @@ public class EmployeeController {
     @GetMapping("/employees/{id}")
     EntityModel<Employee> one(@PathVariable Long id) {
         Employee employee = repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
-        return EntityModel.of(employee,
-                linkTo(methodOn(EmployeeController.class).one((id))).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+        return assembler.toModel(employee);
     }
 
     @PutMapping("/employee/{id}")
